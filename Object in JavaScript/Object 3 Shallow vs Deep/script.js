f09f865b093dc994e29b02d6c2b9fdad9ae4c35f
Srv_x13 <krsaurav1326@gmail.com>
2025-06-30 23:55:19 +0530

    Shallow Copy v/s Deep Copy

--- Object in JavaScript/Object 3 Shallow vs Deep/script.js
+++ Object in JavaScript/Object 3 Shallow vs Deep/script.js
@@ -2,6 +2,8 @@
 
 // SHALLOW COPY - Only copies the first layer (top-level) of an object. Nested objects still point to the same memory.
 
+/* Example 1 - 
+
 const original = {
   name: "Saurav",
   details: {
@@ -14,10 +16,23 @@ const shallowCopy = { ...original };
 shallowCopy.details.age = 25;
 
 console.log(original.details.age); // 25 😱 (changed!)
+*/
 
+/* Example 2 - */
+
+const obj1 = {
+  a : 10,
+  b : 2,
+};
+
+const obj2 = obj1;
+console.log(obj2,obj1);
+obj2.a = 999;
+console.log(obj2,obj1);
 
 // Deep Copy - Copies everything, including all nested objects. So they are completely independent.
 
+/* Example 1 
 const obj = {
   name: "Saurav",
   details: {
@@ -30,3 +45,12 @@ const deepCopy = JSON.parse(JSON.stringify(obj));
 deepCopy.details.age = 25;
 
 console.log(obj.details.age); // 22 😌 (safe!)
+
+*/
+
+/* Example 2 */
+
+const obj3 = structuredClone(obj1);
+console.log(obj3,obj1);
+obj3.a = 34;
+console.log(obj3,obj1);
